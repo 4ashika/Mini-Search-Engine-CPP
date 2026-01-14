07d6a8e6c5ae455a1aaaa468f449e0eb326acebd
Aashika <srivastava.aashika06@gmail.com>
2026-01-15 00:50:36 +0530

    Base version completed

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include "minisearch.h"
-#include "trie.h"
 
 int main() {
     MiniSearch engine;
+    std::cout << "--- Initializing Search Engine ---" << std::endl;
     engine.loadDatasets("./datasets");
 
-    std::string query = "t"; // Test with a prefix
-    std::cout << "User is typing: " << query << "..." << std::endl;
-    
-    engine.autocomplete(query); 
+    int choice;
+    std::string input;
+
+    while (true) {
+        std::cout << "\n1. Search | 2. Autocomplete | 3. Exit\nChoose: ";
+        std::cin >> choice;
+        std::cin.ignore(); // Clear the newline character
+
+        if (choice == 3) break;
+
+        std::cout << "Enter text: ";
+        std::getline(std::cin, input);
+
+        if (choice == 1) {
+            engine.search(input);
+        } else if (choice == 2) {
+            engine.autocomplete(input);
+        }
+    }
 
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -std=c++17 main.cpp minisearch.cpp trie.cpp -o search_engine
